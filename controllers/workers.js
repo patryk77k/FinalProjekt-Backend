7c61daeae7ff283529860f29c0016ff84d285599
patryk77k <patryk77.k@gmail.com>
2023-02-21 18:59:20 +0100

    users workers schema work

--- controllers/workers.js
+++ controllers/workers.js
@@ -1,17 +1,10 @@
+const { findByIdAndUpdate } = require("../models/Worker");
 const Worker = require("../models/Worker");
 
 const getAllWorkers = async (req, res) => {
   try {
     const workers = await Worker.find();
     res.status(200).json(workers);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send(err.message);
-  }
-};
-
-const getSingleWorker = async (req, res) => {
-  try {
   } catch (err) {
     console.log(err);
     res.status(500).send(err.message);
@@ -26,8 +19,45 @@ const createWorker = async (req, res) => {
   }
 };
 
-const updateWorker = async (req, res) => {
+const getSingleWorker = async (req, res) => {
+  const { id } = req.params;
   try {
+    const worker = await Worker.findById(id);
+    res.status(200).json(worker);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
+};
+
+const updateWorker = async (req, res) => {
+  const { id } = req.params;
+  const {
+    first_name,
+    last_name,
+    login,
+    password,
+    profession,
+    experience,
+    availibility,
+    price,
+  } = req.body;
+  try {
+    const updatedWorker = await Worker.findByIdAndUpdate(
+      id,
+      {
+        first_name,
+        last_name,
+        login,
+        password,
+        profession,
+        experience,
+        availibility,
+        price,
+      },
+      { new: true }
+    );
+    res.status(200).json(`${updatedWorker.first_name} ist updated`);
   } catch (err) {
     console.log(err);
     res.status(500).send(err.message);
@@ -35,7 +65,10 @@ const updateWorker = async (req, res) => {
 };
 
 const deleteWorker = async (req, res) => {
+  const { id } = req.params;
   try {
+    const deletedWorker = await Worker.findByIdAndDelete(id);
+    res.status(200).json(`${deletedWorker.first_name} has been deleted`);
   } catch (err) {
     console.log(err);
     res.status(500).send(err.message);
